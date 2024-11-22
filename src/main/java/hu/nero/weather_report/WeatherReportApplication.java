c164f972d562600e492c8b5ec77b018fbcd2d61f
Sergei Nerobeev <sergeinerobeev@gmail.com>
2024-11-22 08:34:49 +0100

    changed .gitignore

--- src/main/java/hu/nero/weather_report/WeatherReportApplication.java
+++ src/main/java/hu/nero/weather_report/WeatherReportApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeatherReportApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WeatherReportApplication.class, args);
 	}
 
