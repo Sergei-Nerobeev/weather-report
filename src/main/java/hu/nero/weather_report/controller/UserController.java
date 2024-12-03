dad9c9c92c3c874b75793fc71d92fc60ee5aa7f5
coder <sergeinerobeev@gmail.com>
2024-12-03 19:39:09 +0100

    init new version

--- src/main/java/hu/nero/weather_report/controller/UserController.java
+++ src/main/java/hu/nero/weather_report/controller/UserController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
-  @GetMapping("/home")
+  @GetMapping("/")
   public String homePage() {
-    return "home";
+    return "index";
   }
 
   @GetMapping("/login")
